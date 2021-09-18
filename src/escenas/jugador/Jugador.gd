5dd785a0cbc947b9feb0b1f758a2b6595ee22dfd
DiegoRutolo <diegorutolo@gmail.com>
2021-09-18 19:05:52 +0200

    Mas animaciones añadidas

--- src/escenas/jugador/Jugador.gd
+++ src/escenas/jugador/Jugador.gd
@@ -21,20 +21,23 @@ func _physics_process(delta):
 	if (Input.is_action_pressed("move_right")):
 		dir += 1
 		animated_sprite.flip_h=false
-		animated_sprite.play();
+		animated_sprite.play("caminar");
 		
 	if (Input.is_action_pressed("move_left")):
 		dir -= 1
 		animated_sprite.flip_h=true
-		animated_sprite.play();
+		animated_sprite.play("caminar");
 		
 	if (dir==0):
 		animated_sprite.stop();
+		animated_sprite.play("quieto")
 		
 		
 	velocity.x = lerp(velocity.x,dir*valor_mov,0.2)
 	
 	if (Input.is_action_just_pressed("jump") and is_on_floor()): 
+		animated_sprite.stop();
+		animated_sprite.play("saltar")
 		velocity.y = -fuerza_salto
 		
 	
