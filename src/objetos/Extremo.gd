1be45e1c9554aa84315004dcc79a587865603af9
Iago Loureiro <iagoloureiroblanco@gmail.com>
2021-09-18 19:10:14 +0200

    Arreglado objeto Extremo

--- src/objetos/Extremo.gd
+++ src/objetos/Extremo.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
 export(NodePath) var pos_tp_path
-onready var pos_tp = get_node(pos_tp_path)
+onready var pos_tp: Position2D = get_node(pos_tp_path)
 
 
 func _on_Extremo_body_entered(body):
-	if body is KinematicBody2D:		# TODO: Cambiar por Actor
-		body.position.x = pos_tp.x
+	if body is Actor:
+		body.position.x = pos_tp.global_position.x
